e8c8515ac4134bd367e36853aaf66df71a04ce29
Sunstar123 <54781339+Sunstar123@users.noreply.github.com>
2021-05-24 19:38:37 -0400

    Update PlayerMovement.cs
    Clean up code. Maintain the same functionality.

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Diagnostics;
@@ -25,24 +25,34 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MoveLogic();
+    }
+
+    void MoveLogic()
+    {
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
-        if (change.x != 0 && change.y != 0) {  // trying to move in both axis at once
-            if (axis == 1) {
+
+        if (change.x != 0 && change.y != 0)  // trying to move in both axis at once?
+        {
+            if (axis == 1)  // already moving in y axis?
+            {
                 change.x = 0;
             }
             else
             {
-                change.y = 0;  // if both directions are tried at the exact same time while stationary, will always go in the x axis
+                change.y = 0; // default to x axis -> if both axis tried at the same time, always go in the x axis
             }
         }
 
         // update axis
-        if (change.x != 0) {
+        if (change.x != 0)
+        {
             axis = -1;
         }
-        else if (change.y != 0) {
+        else if (change.y != 0)
+        {
             axis = 1;
         }
         else
@@ -50,92 +60,30 @@ public class PlayerMovement : MonoBehaviour
             axis = 0;
         }
 
-        UpdateAnimationAndMove();
-
-    }
-
-    void UpdateAnimationAndMove()
-    {
+        UpdateAnimation();
         if (change != Vector3.zero)
         {
             MoveCharacter();
+        }
+    }
+
+    void UpdateAnimation()
+    {
+        if(change != Vector3.zero)
+        {
             animator.SetFloat("moveX", change.x);
             animator.SetFloat("moveY", change.y);
             animator.SetBool("moving", true);
         }
-
         else
         {
             animator.SetBool("moving", false);
         }
     }
 
-
-
     void MoveCharacter()
     {
-        myRigidbody.MovePosition(
+        myRigidbody.MovePosition(  // messes with gravity by smoothly moving toward a point (aka don't use gravity with this)
             transform.position + change * speed * Time.deltaTime);
     }
 }
-
-
-
-//using System.Collections;
-//using System.Collections.Generic;
-//using System.Collections.Specialized;
-//using System.Diagnostics;
-//using System.Security.Cryptography;
-//using System.Threading;
-//using UnityEngine;
-
-//public class PlayerMovement : MonoBehaviour
-//{
-  //  public float speed;
- //   private Rigidbody2D myRigidbody;
- //   private Vector3 change;
- //   private Animator animator;
-
-
-    // Start is called before the first frame update
-  //  void Start()
-    //{
-      //  animator = GetComponent<Animator>();
-  //      myRigidbody = GetComponent<Rigidbody2D>();
-//    }
-
-    // Update is called once per frame
-  //  void Update()
-  //  {
-     //   change = Vector3.zero;
-   //     change.x = Input.GetAxisRaw("Horizontal");
-    //    change.y = Input.GetAxisRaw("Vertical");
-  //      UpdateAnimationAndMove();
-
-//    }
-
-    //void UpdateAnimationAndMove()
-   // {
-     //   if (change != Vector3.zero)
-   //     {
- //           MoveCharacter();
-           // animator.SetFloat("moveX", change.x);
-            //animator.SetFloat("moveY", change.y);
-          //  animator.SetBool("moving", true);
-        //}
-
-        //else
-        //{
-        //    animator.SetBool("moving", false);
-      //  }
-    //}
-
-
-
-    //void MoveCharacter()
-    //{
-    //    myRigidbody.MovePosition(
-     //       transform.position + change * speed * Time.deltaTime);
-  //  }
-//}
-
